129881a1c515e07f0f6903e44d722c485ffd83eb
Vincent Franstyo <sorakiriko28814@gmail.com>
2023-11-08 11:52:01 +0700

    chore(DB): change json to supabase DB

--- healthcare.py
+++ healthcare.py
@@ -2,19 +2,33 @@ import json
 
 from fastapi import APIRouter
 
-from models import HealthFacility, FacilityUpdate
 from db.supabase import create_supabase_client
+from models import HealthFacility, FacilityUpdate
 
 router = APIRouter()
+supabase_client = create_supabase_client()
 
-json_filename = "db/health_facilities.json"
 
-with open(json_filename, "r") as read_file:
-    facilities = json.load(read_file)
+def get_facilities():
+    facs = supabase_client.from_("Health Facilities").select("*").execute()
+    return facs
 
 
 def get_facility_ids():
-    return [facility["facility_id"] for facility in facilities]
+    facs = get_facilities()
+    return [facility["facility_id"] for facility in facs]
+
+
+def save_to_db(healthcares):
+    try:
+        facs = supabase_client.from_("Health Facilities").insert(healthcares).execute()
+        return facs
+    except Exception as e:
+        print("Error inserting")
+        return "Error inserting"
+
+
+facilities = get_facilities()
 
 
 @router.get("/")
@@ -38,58 +52,63 @@ async def get_health_facility_by_id(facility_id: str):
 
 @router.post("/{facility_id}")
 async def create_health_facility(facility_id: str, add_facility: HealthFacility):
-    add_facility.dict()['facility_id'] = facility_id
-    facility_ids = get_facility_ids()
-    if facility_id in facility_ids:
-        return {"message": "The facility with ID: " + facility_id + " is already there"}
+    try:
+        add_facility.dict()['facility_id'] = facility_id
+        facility_ids = get_facility_ids()
+        if facility_id in facility_ids:
+            return {"message": "The facility with ID: " + facility_id + " is already there"}
 
-    for facility in facilities:
-        if add_facility.coordinates.longitude == facility['coordinates']['longitude'] and add_facility.coordinates.latitude == facility['coordinates']['latitude']:
-            return {"message": "The facility at that coordinate already exists"}
+        for facility in facilities:
+            if add_facility.coordinates.longitude == facility['coordinates']['longitude'] and add_facility.coordinates.latitude == facility['coordinates']['latitude']:
+                return {"message": "The facility at that coordinate already exists"}
 
-    add_facility.facility_id = facility_id
-    facilities.append(add_facility.dict())
-    with open(json_filename, "w") as write_file:
-        json.dump(facilities, write_file)
-
-    return {"message": "Facility added successfully"}
+        add_facility.facility_id = facility_id
+        facilities.append(add_facility.dict())
+        facs = save_to_db(facilities)
+        return [{"facilities": facs}, {"message": "Facility added successfully"}] if facs else [
+            {"message": "failed to add facility"}]
+    except Exception as e:
+        print("Error", e)
+        return [{"message": "Error adding facility"}]
 
 
 @router.put("/{facility_id}")
 async def update_health_facility(facility_id: str, update_fac: FacilityUpdate):
-    facility_ids = get_facility_ids()
-    if facility_id not in facility_ids:
-        return {"The facility you are looking for is not available"}
+    try:
+        facility_ids = get_facility_ids()
+        if facility_id not in facility_ids:
+            return {"The facility you are looking for is not available"}
 
-    for facility in facilities:
-        if facility['facility_id'] == facility_id:
-            update_data = {key: value for key, value in update_fac.dict().items() if value}
-            facility.update(update_data)
+        for facility in facilities:
+            if facility['facility_id'] == facility_id:
+                update_data = {key: value for key, value in update_fac.dict().items() if value}
+                facility.update(update_data)
 
-    with open(json_filename, 'w') as update_file:
-        json.dump(facilities, update_file)
-
-    return {"message": "Facilities updated successfully"}
+        facs = save_to_db(facilities)
+        return [{"facilities": facs}, {"message": "Facility updated successfully"}] if facs else [
+            {"message": "failed to update facility"}]
+    except Exception as e:
+        print("Error: ", e)
+        return [{"message": "failed to update facility"}]
 
 
 @router.delete('/{facility_id}')
 async def delete_health_facility(facility_id: str):
-    global facilities
+    healthcares = get_facilities()
     facility_ids = get_facility_ids()
     if facility_id not in facility_ids:
         return {"message": "The facility you are looking for is not available"}
 
     facilities_to_delete = []
-    for facility in facilities:
+    for facility in healthcares:
         if facility['facility_id'] == facility_id:
             facilities_to_delete.append(facility)
     if not facilities_to_delete:
         return {"message": "The facility you are looking for is not available"}
 
     fac_name = facilities_to_delete[0]['facility_name']
-    facilities = [facility for facility in facilities if facility not in facilities_to_delete]
+    healthcares = [facility for facility in healthcares if facility not in facilities_to_delete]
 
-    with open(json_filename, 'w') as delete_file:
-        json.dump(facilities, delete_file)
+    facs = save_to_db(healthcares)
 
     return {"Message": "Healthcare " + fac_name + " deleted successfully"}
